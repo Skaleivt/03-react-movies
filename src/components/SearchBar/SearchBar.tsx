e3778df256a59521a6f204761dce268142a1c3a2
Skaleivt <skaleiviktor@gmail.com>
2025-07-13 11:52:04 +0300

    add normalize

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,1 +1,44 @@
-<></>;
+import styles from "./SearchBar.module.css";
+
+// import toast, { Toaster } from "react-hot-toast";
+
+// const notify = () => toast("Here is your toast.");
+
+// const App = () => {
+//   return (
+//     <div>
+//       <button onClick={notify}>Make me a toast</button>
+//       <Toaster />
+//     </div>
+//   );
+// };
+
+export default function searchBar() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a
+          className={styles.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form className={styles.form}>
+          <input
+            className={styles.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={styles.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "modern-normalize";
 // import "./index.css";
-// import App from "./components/App/App.tsx";
+import App from "./components/App/App.tsx";
 
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
