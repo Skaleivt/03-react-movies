ff2cecdf2916ccd3aedc2f0dbf6d5a882fb523af
Skaleivt <skaleiviktor@gmail.com>
2025-07-15 12:40:57 +0300

    fix movieServise

--- src/service/movieService.ts
+++ src/service/movieService.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-import type { MovieRefs } from "../types/movie";
+import type { MoviePops } from "../types/movie";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 export default async function fetchMovies(query: string) {
   const myKey = import.meta.env.VITE_API_KEY;
 
-  const response = await axios.get<MovieRefs>(`/search/movie`, {
+  const response = await axios.get<MoviePops>(`/search/movie`, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
     },
